5dd36e29204c61b6961714982fc615e5b3379a4b
MarineVega <marinev33@hotmail.com>
2025-11-26 21:14:54 -0300

    volví a modificar el log, para que no muestre undefined

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
 */
   //await app.listen(process.env.PORT ?? 3000);
   await app.listen(3000);
-  console.log('🚀 Backend corriendo en ', process.env.HOST);
+  console.log('🚀 Backend corriendo ');
 }
 bootstrap();
 
